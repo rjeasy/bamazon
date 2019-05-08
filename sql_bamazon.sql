b08b03721d41f3b1a6b5ec7a6c5c14ba4d4aa174
bigez <rjones26@outlook.com>
2019-05-07 22:37:01 -0700

    updating files for project

---
+++ sql_bamazon.sql
@@ -0,0 +1,17 @@
+create table products (
+item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(30),
+department_name VARCHAR(30),
+price DECIMAL(10,2),
+stock_quantity INTEGER(11),
+PRIMARY KEY(item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("soap", "bathroom goods", 1.50, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("soap", "bathroom goods", 1.50, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("soap", "bathroom goods", 1.50, 10);
